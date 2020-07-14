9b3be90e73acbb1c6f15c4c3a4b3f773726a5465
Alpha Niang <alpha.niang@hotmail.com>
2020-07-14 12:31:45 +0200

    ajout de cros(*)

--- src/main/java/fr/isika/microservice_boutique/controller/VetementController.java
+++ src/main/java/fr/isika/microservice_boutique/controller/VetementController.java
@@ -1,6 +1,7 @@
 package fr.isika.microservice_boutique.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import fr.isika.microservice_boutique.model.Vetements;
 import reactor.core.publisher.Flux;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/boutique")
 public class VetementController {
